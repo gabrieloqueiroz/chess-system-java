676d6af6adf09977c0224d18a7162c1ca8ece608
Gabriel Queiroz <gabriel-cso@hotmail.com>
2021-11-02 19:19:58 -0300

    First class: position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args){
 
-        System.out.println("Hello World");
+        Position position = new Position(3, 5);
+
+        System.out.print(position);
 
     }
 }
